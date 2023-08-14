ab74beee09ba63bc8ccf8d7a84739f6ec24d8954
Harsh Chauhan <73985490+harz05@users.noreply.github.com>
2023-08-14 21:22:22 +0530

    Add files via upload

---
+++ app2.js
@@ -0,0 +1,306 @@
+const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+
+
+tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
+
+
+
+
+tl.fromTo(".middle", { opacity: 0 }, { opacity: 1, duration: 1 });
+
+
+tl.fromTo("#myBtn", { opacity: 0 }, { opacity: 1, duration: 1 });
+
+
+/***for gallery added by harz**//*
+tl.fromTo(".row", { opacity: 0 }, { opacity: 1, duration: 1 });
+
+/*------------------------------------timer-------------------------------*/
+
+// Set the date we're counting down to
+var countDownDate = new Date("july 23, 2021 15:37:25").getTime();
+
+// Update the count down every 1 second
+var countdownfunction = setInterval(function() {
+
+  // Get todays date and time
+  var now = new Date().getTime();
+  
+  // Find the distance between now an the count down date
+  var distance = countDownDate - now;
+  
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24))+ "  ";
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))+ "  ";
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))+ "  ";
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000)+ "  ";
+  
+  // Output the result in an element with id="demo"
+  document.getElementById("demo").innerHTML = days + "d   " + hours + "h   "
+  + minutes + "m   " + seconds + "s   ";
+  
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(countdownfunction);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
+
+
+
+/**********************/
+const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".cursor");
+
+const textArray = ["Deliberate.", "Defend.", "Deliver."];
+const typingDelay = 200;
+const erasingDelay = 150;
+const newTextDelay = 2000; // Delay between current and next text
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+    setTimeout(erase, newTextDelay);
+  }
+}
+
+function erase() {
+  if (charIndex > 0) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+    textArrayIndex++;
+    if(textArrayIndex>=textArray.length) textArrayIndex=0;
+    setTimeout(type, typingDelay + 1100);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
+  if(textArray.length) setTimeout(type, newTextDelay + 250);
+});
+
+
+
+
+
+
+
+/* document.addEventListener(
+  "scrollbar",
+  function() {
+    var scrollbarTop =
+      document.documentElement["scrollbarTop"] || document.body["scrollbarTop"];
+    var scrollbarBottom =
+      (document.documentElement["scrollbarHeight"] ||
+        document.body["scrollbarHeight"]) - document.documentElement.clientHeight;
+    scrollbarPercent = scrollbarTop / scrollbarBottom * 100 + "%";
+    document
+      .getElementById("progress_bar")
+      .style.setProperty("--scrollbar", scrollbarPercent);
+  },
+  { passive: true }
+); */
+
+
+/*************************this is a code for the expansion of text-harz********/
+
+var services = document.querySelector('#rowID');
+
+services.addEventListener('click', function(e) {
+  var target = e.target;
+  if (target.classList.contains('btn')) {
+    var serviceItem = target.parentNode;
+    var open = target.dataset.open === 'true';
+    target.dataset.open = !open;
+    if (open) {
+      target.innerText = 'Read more';
+      serviceItem.classList.remove('open');
+    } else {
+      target.innerText = 'Read less';
+      serviceItem.classList.add('open');
+    }
+  }
+});
+
+/*********to top*/
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+/***************************************************/
+
+
+var radius = 240;
+var autoRotate = true;
+var rotateSpeed = -60;
+var imgWidth = 120; 
+var imgHeight = 170;
+
+
+var bgMusicURL = 'https://api.soundcloud.com/tracks/143041228/stream?client_id=587aa2d384f7333a886010d5f52f302a';
+var bgMusicControls = true;
+
+
+
+setTimeout(init, 100);
+
+var obox = document.getElementById('drag-container');
+var ospin = document.getElementById('spin-container');
+var aImg = ospin.getElementsByTagName('img');
+var aVid = ospin.getElementsByTagName('video');
+var aEle = [...aImg, ...aVid];
+
+
+ospin.style.width = imgWidth + "px";
+ospin.style.height = imgHeight + "px";
+
+
+var ground = document.getElementById('ground');
+ground.style.width = radius * 3 + "px";
+ground.style.height = radius * 3 + "px";
+
+function init(delayTime) {
+  for (var i = 0; i < aEle.length; i++) {
+    aEle[i].style.transform = "rotateY(" + (i * (360 / aEle.length)) + "deg) translateZ(" + radius + "px)";
+    aEle[i].style.transition = "transform 1s";
+    aEle[i].style.transitionDelay = delayTime || (aEle.length - i) / 4 + "s";
+  }
+}
+
+function applyTranform(obj) {
+
+  if(tY > 180) tY = 180;
+  if(tY < 0) tY = 0;
+
+  obj.style.transform = "rotateX(" + (-tY) + "deg) rotateY(" + (tX) + "deg)";
+}
+
+function playSpin(yes) {
+  ospin.style.animationPlayState = (yes?'running':'paused');
+}
+
+var sX, sY, nX, nY, desX = 0,
+    desY = 0,
+    tX = 0,
+    tY = 10;
+
+
+if (autoRotate) {
+  var animationName = (rotateSpeed > 0 ? 'spin' : 'spinRevert');
+  ospin.style.animation = `${animationName} ${Math.abs(rotateSpeed)}s infinite linear`;
+}
+
+
+if (bgMusicURL) {
+  document.getElementById('music-container').innerHTML += `
+<audio src="${bgMusicURL}" ${bgMusicControls? 'controls': ''} autoplay loop>    
+<p>If you are reading this, it is because your browser does not support the audio element.</p>
+</audio>
+`;
+}
+
+
+document.onpointerdown = function (e) {
+  clearInterval(obox.timer);
+  e = e || window.event;
+  var sX = e.clientX,
+      sY = e.clientY;
+
+  this.onpointermove = function (e) {
+    e = e || window.event;
+    var nX = e.clientX,
+        nY = e.clientY;
+    desX = nX - sX;
+    desY = nY - sY;
+    tX += desX * 0.1;
+    tY += desY * 0.1;
+    applyTranform(obox);
+    sX = nX;
+    sY = nY;
+  };
+
+  this.onpointerup = function (e) {
+    obox.timer = setInterval(function () {
+      desX *= 0.95;
+      desY *= 0.95;
+      tX += desX * 0.1;
+      tY += desY * 0.1;
+      applyTranform(obox);
+      playSpin(false);
+      if (Math.abs(desX) < 0.5 && Math.abs(desY) < 0.5) {
+        clearInterval(obox.timer);
+        playSpin(true);
+      }
+    }, 17);
+    this.onpointermove = this.onpointerup = null;
+  };
+
+  return false;
+};
+
+document.onmousewheel = function(e) {
+  e = e || window.event;
+  var d = e.wheelDelta / 20 || -e.detail;
+  radius += d;
+  init(1);
+};
+
+/**********************************************websiute reveal***********************************/
+
+let sections = document.querySelectorAll(".section");
+window.onscroll = function fadeIn() {
+    sections.forEach(section => {
+        let sectionSize = section.getBoundingClientRect();
+        let bottomPart = sectionSize.bottom;
+        if (window.scrollY >= bottomPart) {
+            section.style.opacity = "1";
+            section.style.transform = "translateX(0)";
+            section.style.transition = "1s ease-in-out";
+        } else {
+            section.style.opacity = "0";
+            section.style.transform = "translateX(-20px)";
+            section.style.transition = "1s ease-in-out";
+        }
+    });
+}
+
+
+/***********************************************/
+
+
+function myFunction() {
+  var x = document.getElementById("ocgallery");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
